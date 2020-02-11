692f664655e330886375fb54c186fc151f10d5ca
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-11 19:44:29 +0100

    Build walking skeleton - end to end

---
+++ Datify.API/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Datify.API.Data;
+
+namespace Datify.API.Controllers 
+{
+    // POST http://localhost:3000/values
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase {
+        private readonly DataContext _context;
+        public ValuesController (DataContext context) {
+            _context = context;
+
+        }
+
+        // Get api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+    }
+}
---
+++ Datify.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Datify.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Datify.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
