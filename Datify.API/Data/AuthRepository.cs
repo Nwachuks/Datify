7051382e8d848f04631f3d7639aa6b695294672c
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-12 11:49:42 +0100

    Implement login and user exists methods in auth repository

--- Datify.API/Data/AuthRepository.cs
+++ Datify.API/Data/AuthRepository.cs
@@ -1,4 +1,4 @@
-using System;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Datify.API.Models;
 
@@ -30,12 +30,44 @@ namespace Datify.API.Data {
             };
         }
 
-        public Task<User> Login (string username, string password) {
-            throw new System.NotImplementedException ();
+        public async Task<User> Login (string username, string password) {
+            // Find user in database using username
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            // If user doesn't exist
+            if (user == null) {
+                return null;
+            }
+
+            // If password is incorrect
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) {
+                return null;
+            }
+
+            return user;
         }
 
-        public Task<bool> UserExists (string username) {
-            throw new System.NotImplementedException ();
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            // Generate hash using user's password salt
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) {
+                // Compute passwordHash using the given password
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                // Compare both hashes for equality
+                for (int i = 0; i < computedHash.Length; i++) {
+                    if (computedHash[i] != passwordHash[i]) {
+                        return false;
+                    }
+                }
+            };
+            return true;
+        }
+
+        public async Task<bool> UserExists (string username) {
+            if (await _context.Users.AnyAsync(x => x.Username == username)) {
+                return true;
+            }
+            return false;
         }
     }
 }
