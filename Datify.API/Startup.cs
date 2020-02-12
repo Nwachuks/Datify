1df9dc9641a8d7f64c0434a3a37e092e72604aed
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-12 12:20:14 +0100

    Register auth service

--- Datify.API/Startup.cs
+++ Datify.API/Startup.cs
@@ -31,6 +31,7 @@ namespace Datify.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
