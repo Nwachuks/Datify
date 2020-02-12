19b10868d5e04de06a3e87c5a9ea41b89d23b46e
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-12 11:23:54 +0100

    Create auth repository interface

---
+++ Datify.API/Data/IAuthRepository.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Datify.API.Models;
+
+namespace Datify.API.Data {
+    public interface IAuthRepository {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
