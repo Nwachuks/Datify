0bde392a2c3419edf625d9aaef796046de2e2182
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-12 10:42:21 +0100

    Create the User model and User table in DB

--- Datify.API/Data/DataContext.cs
+++ Datify.API/Data/DataContext.cs
@@ -9,5 +9,7 @@ namespace Datify.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) { }
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
