89a3816fe47968817bfec5183cb860ebacf63cf3
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-13 11:21:54 +0100

    Test frontend login/logout functionality

--- Datify-SPA/src/app/nav/nav.component.ts
+++ Datify-SPA/src/app/nav/nav.component.ts
@@ -23,4 +23,14 @@ export class NavComponent implements OnInit {
     // console.log(this.model);
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out!');
+  }
+
 }
