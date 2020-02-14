7ec3e9dec115be2d5083704eccfc14c9712bb5e5
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-14 15:47:17 +0100

    Fix navbar to show appropriate options when logged in

--- Datify-SPA/src/app/app.module.ts
+++ Datify-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
