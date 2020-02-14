55049365fc001282e5ed406103295bd8ed6329de
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-14 20:01:52 +0100

    Extend user model

--- Datify.API/Data/DataContext.cs
+++ Datify.API/Data/DataContext.cs
@@ -11,5 +11,7 @@ namespace Datify.API.Data
         public DbSet<Value> Values { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
