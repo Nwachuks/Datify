f135a093e1e4b49a24470c09f9d5af92adae5807
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-14 17:24:18 +0100

    Change routes after login/logout

--- Datify-SPA/src/app/nav/nav.component.ts
+++ Datify-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 
@@ -10,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,8 @@ export class NavComponent implements OnInit {
     }, error => {
       this.alertify.error('Error in login');
       console.log(error);
+    }, () => {
+      this.router.navigate(['/matches']);
     });
     // console.log(this.model);
   }
@@ -34,6 +37,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('You\'re logged out');
+    this.router.navigate(['/home']);
   }
 
 }
