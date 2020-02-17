5b23e89aaba2d3717237435c52fff00be33edfd9
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-17 09:45:21 +0100

    Retrieve matches list

--- Datify-SPA/src/app/matches-list/matches-list.component.ts
+++ Datify-SPA/src/app/matches-list/matches-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-matches-list',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matches-list.component.css']
 })
 export class MatchesListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
