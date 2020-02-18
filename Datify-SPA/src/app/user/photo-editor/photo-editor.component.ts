6787e94a1c195beaf0de69ad7bf8df4517db32b4
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-18 18:28:12 +0100

    Configure photo upload functionality

--- Datify-SPA/src/app/user/photo-editor/photo-editor.component.ts
+++ Datify-SPA/src/app/user/photo-editor/photo-editor.component.ts
@@ -1,8 +1,8 @@
-import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import { environment } from './../../../environments/environment';
 import { Photo } from './../../_models/photo';
+import { environment } from './../../../environments/environment';
+import { AuthService } from './../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -35,6 +35,21 @@ export class PhotoEditorComponent implements OnInit {
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
     });
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
 }
