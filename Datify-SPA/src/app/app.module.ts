029bfb7a077cac07b3a51a1c6e6b288594f39025
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-21 08:59:57 +0100

    Format dates for created and last-active properties

--- Datify-SPA/src/app/app.module.ts
+++ Datify-SPA/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,7 +46,8 @@ export function tokenGetter() {
     LikesListComponent,
     MessagesComponent,
     ProfileComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
