f4e760c1d241d58909ff73787fe77dca9ffb5968
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-02-24 12:27:35 +0100

    Configure like functionality in client

--- Datify-SPA/src/app/_services/user.service.ts
+++ Datify-SPA/src/app/_services/user.service.ts
@@ -66,4 +66,7 @@ export class UserService {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
 
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
 }
--- Datify-SPA/src/app/matches/match-card/match-card.component.ts
+++ Datify-SPA/src/app/matches/match-card/match-card.component.ts
@@ -1,3 +1,6 @@
+import { AlertifyService } from './../../_services/alertify.service';
+import { UserService } from './../../_services/user.service';
+import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from './../../_models/user';
 
@@ -9,9 +12,17 @@ import { User } from './../../_models/user';
 export class MatchCardComponent implements OnInit {
   @Input() user: User;
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+  }
+
+  sendLike(id: number) {
+    this.userService.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
+      this.alertify.success('You have liked: ' + this.user.fullName);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
